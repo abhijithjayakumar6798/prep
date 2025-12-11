050b3d1ed14286173334db940c671f50aa2dcdcc
abhijithjayakumar6798 <abhijithjayakumarofficial@gmail.com>
2025-12-11 08:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (67.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        
+        ch=set(s)
+
+        for i in ch:
+            if s.count(i) != t.count(i):
+                return False
+        
+        return True
